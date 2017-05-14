945480c521bf8de9248c916279e7ebb9c7a314df
Felipe Santos <fdms@icomp.ufam.edu.br>
2017-05-14 16:15:39 -0400

    Alguns comentários para não me perder quando voltar ao trabalho

--- src/CirculacaoDAO.java
+++ src/CirculacaoDAO.java
@@ -2,10 +2,11 @@ import java.sql.*;
 
 public class CirculacaoDAO extends BancoDeDados{
 	
-	
+	//Atualizar valor total da circulacao
+	//Cadastro de data de inicio da circulacao
 	public static void main(String[] args) {
 		
-		
+		//Testando formatação de produtos no banco
 		Produto p = new Produto("mdx", 1, "produto bacana, serve pra um monte de coisa.", 1.02);
 		
 		System.out.println(p.getPrecoFinal());
--- src/ProdutoCirculandoDAO.java
+++ src/ProdutoCirculandoDAO.java
@@ -2,7 +2,8 @@ import java.sql.*;
 
 public class ProdutoCirculandoDAO extends BancoDeDados{
 
-	
+	//Dizer individualmente quantos produtos de cada tipo cadastrado está em ciruculação
+	//Atualizar quantidade de vendidos a cada ciruculacao
 	
 	public static void main(String[] args) {
 		
